88811344ff8cf295e1b3282e0b08987848b7c7fa
Gisele Byrne <giselebyrne@gmail.com>
2022-11-30 05:42:55 -0300

    Inclusão do conteúdo final de Operadores

--- Trilha_Java_Basico/tipos-variaveis/src/Operadores.java
+++ Trilha_Java_Basico/tipos-variaveis/src/Operadores.java
@@ -38,8 +38,59 @@ public class Operadores {
         // Negando uma variável Booleana com '!'
         boolean variavel = true;
         variavel = !variavel;
-
         System.out.println(variavel);
 
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        // EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        // String resultado = "";
+        // if (a == b)
+        // resultado = "verdadeiro";
+        // else
+        // resultado = "falso";
+
+        // Operador Ternario
+        String resultadoS = a == b ? "verdadeiro" : "falso";
+        int resultado = a == b ? 1 : 2;
+        System.out.println(resultadoS);
+        System.out.println(resultado);
+
+        // Operador Relacional
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        if(numero1 < numero2){
+            System.out.println("A nossa condição é verdadeira!");
+        }
+
+        System.out.println("Numero 1 é igual a numero 2? " + simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println("Numero 1 é diferente a numero 2? " + simNao);
+
+        simNao = numero1 > numero2;
+        System.out.println("Numero 1 é maior que numero 2? " + simNao);
+
+        simNao = numero1 >= numero2;
+        System.out.println("Numero 1 é maior ou igual a numero 2? " + simNao);
+
+        String nomeUm = "Gisele";
+        //String nomeDois = "Gisele";
+        //System.out.println(nomeUm == nomeDois);
+        String nomeDois = new String("Gisele");
+        System.out.println(nomeUm.equals(nomeDois));
+
+        // Operadores  Lógicos && -> 'e' ... || -> 'ou'
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2){
+        System.out.println("As duas condições são verdadeiras!");
+        }
+        
     }
 }
